4115067fdd8eb79589bc7850eb25a5fad00f9fc0
TOMAS FERNANDEZ ZABALA <47394082@ort>
2024-05-29 12:13:56 -0300

    s

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import usersList from './components/usersList.jsx';
+import './App.css';
+
+function App() {
+  return (   
+      <usersList/>
+      <>
+      <div className="model-usuario"></div>
+      </>
+  );
+}
+
+export default App;
